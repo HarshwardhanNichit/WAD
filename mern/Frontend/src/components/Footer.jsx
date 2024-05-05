8037368d4e2039b6722b0bbad5e465ae735010b2
Harshwardhan Nichit <harshwardhannichit@gmail.com>
2024-05-05 16:53:43 +0530

    Commit1

---
+++ mern/Frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="banner">
+                    <div className="left">Please visit Us!</div>
+                    <div className="right">
+                        <p>Near Indira Nagar, Nashik, Maharashtra</p>
+                        <p>Open: 10:00 AM - 10:00 PM</p>
+                    </div>
+                </div>
+                <div className="banner">
+                    <div className="left">
+                        <p>Developed by Harshwardhan Nichit</p>
+                    </div>
+                    <div className="right">
+                        <p>All Rights Reserved by Harshwardhan Nichit</p>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
